7369963b7514ceca6eed72a6a38afbc5e85a0577
R1maro <amir.mmdi1221@gmail.com>
2023-05-19 01:30:56 +0330

    creat a user and some posts in your database table we return all posts in PostController.php index function with response method u can see all posts returned in json value in postman app(Headers/Content-Type)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,6 +13,10 @@ class PostController extends Controller
 //            'title' => 'test'
 //        ];
 
-        return response()->json('test');
+//        return response()->json('test');
+
+        return response()->json(Post::all());
     }
 }
+
+
