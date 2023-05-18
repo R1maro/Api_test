f35accaf0e94ec5d4d2975491b3e96f63fef39ed
R1maro <amir.mmdi1221@gmail.com>
2023-05-19 01:14:13 +0330

    migration foreign key problem fixed created PostController & index function for test now we create a Api endpoint(Route) in api.php u have to install "Postman" application or some apps like this for see json response from server or client now you can see when you return 'test' in array type in index function our Content-Type in Postman is application/json

--- database/migrations/2023_05_18_210912_create_posts_table.php
+++ database/migrations/2023_05_18_210912_create_posts_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
 
             $table->foreignId('user_id');
             $table->foreign('user_id')->references('id')
-                ->on('user')->onDelete('cascade');
+                ->on('users')->onDelete('cascade');
 
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::get('/posts',[\App\Http\Controllers\PostController::class,'index']);
